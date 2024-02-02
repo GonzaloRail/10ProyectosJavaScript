2088e6bdd56e26cd1def81b050a1986175c39638
GonzaloRail <glaymem@unsa.edu.pe>
2024-02-02 00:37:28 -0500

    Se creo index y script incompleto

--- AppDePomodoro/script.js
+++ AppDePomodoro/script.js
@@ -3,9 +3,44 @@ let time = 0;
 let timer = null;
 let timerBreak = null;
 let current = null;
-
+//Boton de enviar
 const bAdd = document.querySelector("#bAdd");
+//cuadro de texto
 const itTask = document.querySelector("#itTask");
 const form = document.querySelector("#form");
 
 
+form.addEventListener('submit', e=>{
+    e.preventDefault();
+    if(itTask. value !='')
+        createTask(itTask.value);
+        itTask. value = '';
+        renderTasks();
+});
+
+function createTask(value){
+
+}
+
+function createTask(value) {
+    const newTask ={
+        id: (Math.random()* 100).toString(36).slice(3),
+        title: value,
+        completed: false,
+  
+    };
+    
+    tasks.unshift(newTask);
+}
+
+function renderTasks(){
+    const html = tasks.map(task => {
+        return `
+            <div class="task"> ´´
+            <!-- Contenido de la tarea -->
+            </div>;
+        `;
+
+    });
+    
+} 
